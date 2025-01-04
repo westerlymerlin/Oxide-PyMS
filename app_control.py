0feebde8621242492b1d4ff97e207a78a3a835e2
Gary Twinn <gary@twinn.net>
2025-01-04 13:50:14 +0000

    1.0.10  Change to Logging Format

--- app_control.py
+++ app_control.py
@@ -7,9 +7,9 @@ calculating a file name and removing illegal character.
 import json
 import datetime
 
-VERSION = '1.0.9'
+VERSION = '1.0.10'
 running = True
-alarms = {'laserhost': 0, 'valvehost': 0, 'xyhost': 0, 'pumphost': 0, 'hidenhost': 0, 'laseralarm': 133}
+alarms = {'laserhost': 0, 'valvehost': 0, 'xyhost': 0, 'pumphost': 0, 'hidenhost': 0, 'laseralarm': 0}
 
 
 def friendlydirname(sourcename: str) -> str:
