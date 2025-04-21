8b0a2cfbc5d51dc6972634f09bef5610f4af4f75
Gary Twinn <gary@twinn.net>
2025-04-21 09:43:02 +0100

    1.2.1   Changed Main Screen to be representative of the physical layout

--- ui/main_form.py
+++ ui/main_form.py
@@ -188,13 +188,13 @@ class UiMain(QMainWindow, Ui_MainWindow):
                 self.secondincrement = 0
                 self.run = 0
                 self.tbRun.setChecked(False)
-        # if alarms['laseralarm'] != 0:
-        #    status = status + ('The laser is not ready, please ensure that the controller laser is powered on, '
-        #                       'the key is in the on position and the door is closed. \n')
-        #    lasergetstatus()
-        #    self.secondincrement = 0
-        #    self.run = 0
-        #    self.tbRun.setChecked(False)
+        if alarms['laseralarm'] != 0:
+            status = status + ('The laser is not ready, please ensure that the controller laser is powered on, '
+                              'the key is in the on position and the door is closed. \n')
+            lasergetstatus()
+            self.secondincrement = 0
+            self.run = 0
+            self.tbRun.setChecked(False)
         if alarms['valvehost'] > 10:
             status = status + 'Valve controller is offline, the system is paused. \n'
             self.secondincrement = 0
