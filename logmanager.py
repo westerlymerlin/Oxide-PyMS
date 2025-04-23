6e0b54db99834797d1e14231762e223350acc76c
Gary Twinn <gary@twinn.net>
2025-04-23 15:55:14 +0100

    Documentation Updates

--- logmanager.py
+++ logmanager.py
@@ -1,6 +1,28 @@
 """
-logmanager, setus up application logging. use the logger property to
-write to the log.
+Logging configuration and management module.
+
+This module sets up application-wide logging with rotating file handlers based on settings.
+It creates the necessary log directory if it doesn't exist and configures logging with
+the following features:
+
+- Rotating log files with size limit of 1MB and 10 backup files
+- Timestamp and log level in each entry
+- Configurable log level (DEBUG/INFO) via settings
+- Log format: [timestamp] - [level] - message
+
+Usage:
+    from logmanager import logger
+
+    logger.debug('Debug message')
+    logger.info('Info message')
+    logger.warning('Warning message')
+    logger.error('Error message')
+
+Configuration:
+    Logging settings are read from settings dictionary with the following keys:
+    - logging.logfilepath: Path to log file directory
+    - logging.logappname: Application name used in log file
+    - logging.loglevel: Logging level (DEBUG/INFO)
 """
 import sys
 import os
@@ -21,7 +43,7 @@ logger = logging.getLogger(settings['logging']['logappname'])
 **logger.debug('message')** for debugging info"""
 
 
-if settings['logging']['level'].upper() == 'DEBUG':
+if settings['logging']['loglevel'].upper() == 'DEBUG':
     logger.setLevel(logging.DEBUG)
 else:
     logger.setLevel(logging.INFO)
@@ -33,4 +55,4 @@ LogFile.setFormatter(formatter)
 logger.addHandler(LogFile)
 
 logger.info('Runnng Python %s on %s', sys.version, sys.platform)
-logger.info('Logging level set to: %s',settings['logging']['level'].upper())
+logger.info('Logging level set to: %s',settings['logging']['loglevel'].upper())
